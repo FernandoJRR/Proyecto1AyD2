5788e4883b11fb6a44f412d1f6a85409c3b0a900
FernandoJRR <fercitotron@gmail.com>
2025-03-11 17:15:58 -0600

    feat: add tailwind and primevue dependencies

---
+++ app_proyecto1_ayd2/app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hola Mundo</h1>
+  <div class="grid grid-cols-3 gap-4 bg-red-400">
+    <Button label="Prueba"/>
+    <Button label="Prueba"/>
+    <Button label="Prueba"/>
+  </div>
+</template>
--- app_proyecto1_ayd2/nuxt.config.ts
+++ app_proyecto1_ayd2/nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+import Aura from "@primeuix/themes/aura"
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  srcDir: 'app/',
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue:{
+    options: {
+      theme: {
+        preset: Aura,
+        cssLayer: {
+          name: 'primevue',
+          order: 'tailwind-base, primevue, tailwind-utilities'
+        }
+      }
+    }
+  },
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
