31927aee9562e13dc5ab9e899b89cf15150574d1
FernandoJRR <fercitotron@gmail.com>
2025-03-12 01:03:56 -0600

    feat: add basic list for personel

--- app_proyecto1_ayd2/app/pages/admin/personal/index.vue
+++ app_proyecto1_ayd2/app/pages/admin/personal/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    
-  </div>
+  <DataTable :value="state.data as any[]" tableStyle="min-width: 50rem" :loading="asyncStatus == 'loading'">
+    <template #header>
+        <div class="flex flex-wrap items-center justify-between gap-2">
+            <span class="text-xl font-bold">Usuarios</span>
+            <Button icon="pi pi-refresh" rounded raised />
+        </div>
+    </template>
+    <Column field="id" header="ID"></Column>
+    <Column field="name" header="Name"></Column>
+    <template #footer> Hay en total {{ state.data ? (state.data as any[]).length : 0 }} usuarios. </template>
+</DataTable>
 </template>
+<script setup lang="ts">
+const { state, asyncStatus } = useQuery({
+  key: ['usuarios'],
+  query: () => $api('/names')
+})
+</script>
