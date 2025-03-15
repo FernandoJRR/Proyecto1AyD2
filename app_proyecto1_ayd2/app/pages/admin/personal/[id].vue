21139e52158b7e7c05ca4eaa746f07c772c1a038
FernandoJRR <fercitotron@gmail.com>
2025-03-15 12:22:31 -0600

    feat: add history structure on employee detail

--- app_proyecto1_ayd2/app/pages/admin/personal/[id].vue
+++ app_proyecto1_ayd2/app/pages/admin/personal/[id].vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-6 ml-12">
     <router-link to="/admin/personal">
-      <Button label="Ver Todos" icon="pi pi-arrow-left" text/>
+      <Button label="Ver Todos" icon="pi pi-arrow-left" text />
     </router-link>
     <div v-if="state.status === 'pending'" class="flex flex-col">
       Cargando...
@@ -9,26 +9,32 @@
     <div v-else-if="state.status === 'error'">
       Ocurrio un error inesperado
     </div>
-    <div v-else class="flex flex-col gap-4">
-      <div class="flex flex-row gap-4 mt-4">
-        <h1 class="text-3xl font-semibold">{{ `${state.data.firstName} ${state.data.lastName}` }}</h1>
-        <Tag :value="state.data.employeeType.name"/>
+    <div v-else class="grid grid-cols-2">
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-row gap-4 mt-4">
+          <h1 class="text-3xl font-semibold">{{ `${state.data.firstName} ${state.data.lastName}` }}</h1>
+          <Tag :value="state.data.employeeType.name" />
+        </div>
+        <div class="flex flex-row gap-x-2 text-lg">
+          <p class="font-medium">Salario Actual:</p>
+          <p class="font-semibold">Q.{{ state.data.salary }}</p>
+        </div>
+        <h1 class="text-xl font-medium">Descuentos</h1>
+        <div class="flex ">
+          <p>IGGS
+            <Tag :severity="state.data.iggsPercentage ? 'success' : 'danger'">
+              {{ state.data.iggsPercentage ? `${state.data.iggsPercentage}%` : 'No Aplica' }}</Tag>
+          </p>
+        </div>
+        <div>
+          <p>IRTRA
+            <Tag :severity="state.data.irtraPercentage ? 'success' : 'danger'">
+              {{ state.data.irtraPercentage ? `${state.data.irtraPercentage}%` : 'No Aplica' }}</Tag>
+          </p>
+        </div>
       </div>
       <div>
-        <h1>Salario Actual: Q.{{ state.data.salary }}</h1>
-      </div>
-      <h1 class="text-xl font-medium">Descuentos</h1>
-      <div class="flex gap-y-8">
-        <p>IGGS 
-          <Tag :severity="state.data.iggsPercentage ? 'success' : 'danger'">
-            {{ state.data.iggsPercentage ? `${state.data.iggsPercentage}%` : 'No Aplica' }}</Tag>
-        </p>
-      </div>
-      <div>
-        <p>IRTRA 
-          <Tag :severity="state.data.irtraPercentage ? 'success' : 'danger'">
-            {{ state.data.irtraPercentage ? `${state.data.irtraPercentage}%` : 'No Aplica' }}</Tag>
-        </p>
+        <p class="text-3xl font-medium">Historial</p>
       </div>
     </div>
   </div>
