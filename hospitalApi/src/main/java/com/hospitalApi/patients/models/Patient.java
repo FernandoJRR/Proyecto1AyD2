ed5a199f9decdcbfcb5f3aaa17322f89ef8533b9
Benjamin <carlosbpac@gmail.com>
2025-03-17 16:03:30 -0600

    Add Patient model with validation and inheritance from Auditor

--- hospitalApi/src/main/java/com/hospitalApi/patients/models/Patient.java
+++ hospitalApi/src/main/java/com/hospitalApi/patients/models/Patient.java
@@ -1,5 +1,49 @@
 package com.hospitalApi.patients.models;
 
-public class Patient {
-    
+import com.hospitalApi.shared.models.Auditor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "paciente")
+public class Patient extends Auditor {
+
+    @NotBlank(message = "El nombre del paciente es requerido")
+    @Column(nullable = false, length = 250)
+    private String firstnames;
+
+    @NotBlank(message = "Los apellidos del paciente son requeridos")
+    @Column(nullable = false, length = 250)
+    private String lastnames;
+
+    @NotBlank(message = "El número de DPI del paciente es requerido")
+    @Pattern(regexp = "\\d{13}", message = "El número de DPI debe contener solo números y tener 13 dígitos")
+    @Column(unique = true, nullable = false, length = 13)
+    private String dpi;
+
+    public Patient(String id, String firstnames, String lastnames, String dpi) {
+        super(id);
+        this.firstnames = firstnames;
+        this.lastnames = lastnames;
+        this.dpi = dpi;
+    }
+
+    public Patient(String firstnames, String lastnames, String dpi) {
+        super();
+        this.firstnames = firstnames;
+        this.lastnames = lastnames;
+        this.dpi = dpi;
+    }
+
+    public Patient updateFromDTO() {
+        return this;
+    }
 }
