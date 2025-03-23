a440bf808ba68211f80251a04035585f677a9560
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2025-03-22 22:04:44 -0600

    feat: add DTOs for SpecialistEmployee creation and update with validation constraints

---
+++ hospitalApi/src/main/java/com/hospitalApi/employees/dtos/SpecialistEmpleoyeeResponseDTO.java
@@ -0,0 +1,11 @@
+package com.hospitalApi.employees.dtos;
+
+import lombok.Value;
+
+@Value
+public class SpecialistEmpleoyeeResponseDTO {
+    private String id;
+    private String nombres;
+    private String apellidos;
+    private String dpi;
+}
