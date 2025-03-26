b2f7c2121a48e02868aeedb6b6d90d28fa7f95aa
Luis Monterroso <elrincondelgamer77@gmail.com>
2025-03-26 04:12:31 -0600

    Fix global handler exception

--- hospitalApi/src/main/java/com/hospitalApi/shared/utils/GlobalExceptionHandler.java
+++ hospitalApi/src/main/java/com/hospitalApi/shared/utils/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import org.springframework.security.access.AccessDeniedException;
 
+import com.hospitalApi.shared.dtos.ErrorResponseDTO;
 import com.hospitalApi.shared.exceptions.DuplicatedEntryException;
 import com.hospitalApi.shared.exceptions.NotFoundException;
 
@@ -25,63 +26,67 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicatedEntryException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleDuplicatedEntryException(DuplicatedEntryException ex) {
-        return ex.getMessage();
+    public ErrorResponseDTO handleDuplicatedEntryException(DuplicatedEntryException ex) {
+        return new ErrorResponseDTO(ex.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleNotFoundException(IllegalStateException ex) {
-        return ex.getMessage();
+    public ErrorResponseDTO handleNotFoundException(IllegalStateException ex) {
+        return new ErrorResponseDTO(ex.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleConstraintViolationException(ConstraintViolationException ex) {
-        return "Error inesperado en la Base de Datos.";
+    public ErrorResponseDTO handleConstraintViolationException(ConstraintViolationException ex) {
+        return new ErrorResponseDTO("Error inesperado en la Base de Datos.");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ErrorResponseDTO handleValidationExceptions(MethodArgumentNotValidException ex) {
         String menssage = "";
         // recorre los errores de validación y agregarlos al mensaje de respuesta
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             menssage = menssage + String.format("- %s \n", error.getDefaultMessage());
         }
-        return menssage;
+
+        return new ErrorResponseDTO(menssage);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public String handleBadCredentialsException(BadCredentialsException ex) {
-        return "Autenticación fallida, El correo electrónico o la contraseña son incorrectos."
-                + " Por favor, verifica tus credenciales e intenta de nuevo.";
+    public ErrorResponseDTO handleBadCredentialsException(BadCredentialsException ex) {
+        return new ErrorResponseDTO("Autenticación fallida, El correo electrónico o la contraseña son incorrectos."
+                + " Por favor, verifica tus credenciales e intenta de nuevo.");
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String handleAccessDeniedException(AccessDeniedException ex) {
-        return "Acceso denegado, no tienes permisos suficientes para realizar esta acción.";
+    public ErrorResponseDTO handleAccessDeniedException(AccessDeniedException ex) {
+        return new ErrorResponseDTO("Acceso denegado, no tienes permisos suficientes para realizar esta acción.");
+
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleUsernameNotFoundException(UsernameNotFoundException ex) {
-        return "El nombre de usuario especificado no pertenece a ningun usuario dentro del sistema.";
+    public ErrorResponseDTO handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return new ErrorResponseDTO(
+                "El nombre de usuario especificado no pertenece a ningun usuario dentro del sistema.");
+
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(NotFoundException ex) {
-        return ex.getMessage();
+    public ErrorResponseDTO handleNotFoundException(NotFoundException ex) {
+        return new ErrorResponseDTO(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleGlobalException(Exception ex) {
+    public ErrorResponseDTO handleGlobalException(Exception ex) {
         ex.printStackTrace();
-        return "Ocurrió un error inesperado: " + ex.getMessage();
+        return new ErrorResponseDTO("Ocurrió un error inesperado: " + ex.getMessage());
     }
 
 }
